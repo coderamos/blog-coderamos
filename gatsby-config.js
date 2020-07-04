589c8805eb6a671b8b12254d84d9f3de9ba0c009
Diego Ramos <diego.coderamos@gmail.com>
2020-07-03 21:38:00 -0300

    update project structure

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `blog-coderamos`,
-    description: `some description...`,
+    description: `A blog about frontend development, UX and UI...`,
     author: `@coderamos`,
   },
   plugins: [
@@ -27,8 +27,5 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
---
+++ src/components/profile/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Profile = () => (
+  <div className="profileWrapper">
+    <h1>Diego Ramos</h1>
+    <h2>front end developer</h2>
+    <p>
+      desenvolvedor front end com experiência em react, javascript, mongodb,
+      express e node. também possuo experiência com ux e metodologias ágeis.
+    </p>
+  </div>
+)
+
+export default Profile
